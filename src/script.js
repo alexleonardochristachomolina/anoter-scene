c0831aa8ae4890151a7ccbb02c18ceb8c406795d
alexleonarodocristanchomolina <alexleonarodocristanchomolina@gmail.com>
2022-08-26 19:23:59 +0100

    secong-

--- src/script.js
+++ src/script.js
@@ -1,8 +1,11 @@
 import './style.css'
 import * as THREE from 'three'
+import { Mesh, Scene } from 'three';
+import gsap from 'gsap'
 
 
 //console.log(THREE)
+
 
 
 
@@ -18,24 +21,57 @@ const size = {
 const set = new THREE.Scene()
 
 //Blue Cube
-const box = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial({color: 0x7492B9});
-const mesh = new THREE.Mesh(box, material);
-mesh.position.z = 1
-mesh.position.y = 0
-mesh.position.x = 0
-set.add(mesh)
+// const box = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial({color: 0x7492B9});
+// const mesh = new THREE.Mesh(box, material);
+// mesh.position.z = 1
+// mesh.position.y = 0
+// mesh.position.x = 0
+// set.add(mesh)
 
+const group = new THREE.Group()
+group.position.y = -1
+group.rotation.y = 1
+set.add(group)
 
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff00ff})
+)
+cube1.position.y = 1
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff2a})
+)
+cube2.position.x = 2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xffff2a})
+)
+cube3.position.x = -2
+group.add(cube3)
 
 //Camera
 const eye = new THREE.PerspectiveCamera(75, size.width/size.height);
 eye.position.z = 3
-eye.position.y = 1
-eye.position.x = 1
 set.add(eye)
 
-mesh.position.set(1,-1,0)
+//eye.lookAt(mesh.position)
+//new THREE.Vector3(3,0,0)
+
+// mesh.position.set(0.7,0.5,1)
+// mesh.scale.set(0.5,0.5,1)
+
+//Rotation
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+
 
 //Axes Helper
 const helper = new THREE.AxesHelper()
@@ -48,6 +84,37 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setSize(size.width, size.height)
-renderer.render(set, eye)
 
+//Time
+//let time = Date.now()
+//const clock = new THREE.Clock()
+
+
+
+
+gsap.to(cube1.position, {duration: 1, delay: 4, y:0})
+gsap.to(cube3.position, {duration: 1, delay: 2, x:-1})
+gsap.to(cube2.position, {duration: 1, delay: 3, x:1})
+//Animation
+const tickTock = () => 
+{
+    //const elapsedTime = clock.getElapsedTime()
+   // console.log(elapsedTime)
+   // const currentTime = Date.now()
+   // const deltaTime = currentTime - time
+   // time = currentTime
+
+   // console.log(deltaTime)
+    //Update Object
+    //cube3.rotation.y = elapsedTime * Math.PI * 2
+   // eye.position.y = Math.sin(elapsedTime)
+   // eye.position.x = Math.sin(elapsedTime)
+   // eye.lookAt(cube3.position)
+    //Render 
+    renderer.render(set, eye)
+
+    window.requestAnimationFrame(tickTock)
+}
+
+tickTock()
 
